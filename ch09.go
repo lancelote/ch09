dbb15e931c753e91a5fdc8d28ea853c7c926eb99
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-05-27 17:51:38 +0200

    make `Add` generic

--- ch09.go
+++ ch09.go
@@ -1,10 +1,18 @@
 package ch09
 
-// Add adds two integer values together and returns the result
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add adds two numerical values together and returns the result
 //
 // For more information see [Math is fun]
 //
 // [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module github.com/lancelote/ch09
+module github.com/lancelote/ch09/v2
 
-go 1.23
+go 1.23.0
+
+toolchain go1.23.5
+
+require golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6
