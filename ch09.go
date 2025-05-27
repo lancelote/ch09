c36db2f19bce11ec88d273bc41bba2b1716d7611
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-05-27 17:39:43 +0200

    ad doc comment to `Add`

--- ch09.go
+++ ch09.go
@@ -1,5 +1,10 @@
 package ch09
 
+// Add adds two integer values together and returns the result
+//
+// For more information see [Math is fun]
+//
+// [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
